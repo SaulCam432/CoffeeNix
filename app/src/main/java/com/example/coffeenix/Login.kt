cde3bda62c6220aa1a1fb146b15aed2d1487f0b4
SaulCam432 <saulcamacho432@gmail.com>
2023-10-17 13:16:54 -0600

    Agregando vinculación de vistas con View Binding

--- app/src/main/java/com/example/coffeenix/Login.kt
+++ app/src/main/java/com/example/coffeenix/Login.kt
@@ -2,10 +2,13 @@ package com.example.coffeenix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coffeenix.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
