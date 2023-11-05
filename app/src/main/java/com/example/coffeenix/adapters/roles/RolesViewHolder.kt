7a7f22d5faf69f2c0c128a126b816167f64b7c6d
SaulCam432 <saulcamacho432@gmail.com>
2023-11-04 19:17:57 -0600

    Agregando nuevamnete las funciones para seleccionar un rol

--- app/src/main/java/com/example/coffeenix/adapters/roles/RolesViewHolder.kt
+++ app/src/main/java/com/example/coffeenix/adapters/roles/RolesViewHolder.kt
@@ -1,8 +1,12 @@
 package com.example.coffeenix.adapters.roles
 
 import android.app.Activity
+import android.content.Intent
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.coffeenix.activities.Cliente.home.ClientHomeActivity
+import com.example.coffeenix.activities.admin.home.AdminHomeActivity
+import com.example.coffeenix.activities.delivery.home.DeliveryHomeActivity
 import com.example.coffeenix.databinding.CardviewRolesBinding
 import com.example.coffeenix.models.Rol
 import com.example.coffeenix.utils.SharedPref
@@ -11,11 +15,38 @@ import com.squareup.picasso.Picasso
 class RolesViewHolder (view: View): RecyclerView.ViewHolder(view) {
     private val binding = CardviewRolesBinding.bind(view)
 
+
     fun bind(context: Activity, rol: Rol){
-        val sharedPref = SharedPref(context)
 
         binding.cardViewRolesText.text = rol.rol
         Picasso.get().load(rol.image).into(binding.cardViewRolesImage);
 
+
+        binding.cardViewRolesBtn.setOnClickListener{
+            goToRol(context, rol)
+            //context = it.context
+        }
+
     }
+
+    private fun goToRol(context: Activity, rol: Rol){
+        val sharedPref = SharedPref(context)
+        if (rol.rol == "ADMIN"){
+            sharedPref.save("rol", "ADMIN")
+            val i = Intent(context, AdminHomeActivity::class.java)
+            context.startActivity(i)
+        }
+        else if (rol.rol == "CLIENTE"){
+            sharedPref.save("rol", "CLIENTE")
+            val i = Intent(context, ClientHomeActivity::class.java)
+            context.startActivity(i)
+        }
+        else if (rol.rol == "REPARTIDOR"){
+            sharedPref.save("rol", "REPARTIDOR")
+            val i = Intent(context, DeliveryHomeActivity::class.java)
+            context.startActivity(i)
+        }
+    }
+
+
 }
